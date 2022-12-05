8cb25583c2542a7bee9b5800fdd443987d53510f
Dom Crayford <domcrayford@gmail.com>
2022-12-05 08:52:36 +0000

    Day 4: Camp cleanup

---
+++ 4/b.py
@@ -0,0 +1,18 @@
+def parse_section(section):
+    pairs = section.strip().split(",")
+    return [[int(value) for value in pair.split("-")] for pair in pairs]
+
+
+def is_in_range(i, p):
+    return i >= p[0] and i <= p[1]
+
+
+def is_overlapping(p1, p2):
+    first_in_second = is_in_range(p1[0], p2) or is_in_range(p1[1], p2)
+    second_in_first = is_in_range(p2[0], p1) or is_in_range(p2[1], p1)
+    return first_in_second or second_in_first
+
+
+sections = [parse_section(section) for section in (open("4/input").readlines())]
+
+print(len([section for section in sections if is_overlapping(section[0], section[1])]))
